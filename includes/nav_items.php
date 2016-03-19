a311baabe20719511215cea704e0e9e2e15207ed
Connor Katz <connor@samuelhubbard.com>
2016-03-19 14:45:12 -0700

    added project files

---
+++ includes/nav_items.php
@@ -0,0 +1,5 @@
+<li><a href="#">Mens</a></li>
+<li><a href="#">Womens</a></li>
+<li><a href="#">Accessoreis</a></li>
+<li><a href="#">Locations</a></li>
+<li><a href="#">Craft</a></li>
--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-this is stuff
+<?php include 'includes/header.php'; ?>
+
+<section id="site_content_container">
+    sadfsadf
+</section><!-- end site content container -->
+
+<?php include 'includes/footer.php'; ?>
